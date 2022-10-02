9998b51909bb68092948ee1418443c4748714ebc
sautedman <bowersd@email.arizona.edu>
2022-10-02 13:19:48 -0700

    removed superfluous function and import statement

--- weight_learner.py
+++ weight_learner.py
@@ -1,5 +1,3 @@
-import harmonic_grammar as hg
-
 def erc(v_winner, v_loser):
     h = []
     for i in range(len(v_winner)):
@@ -7,10 +5,6 @@ def erc(v_winner, v_loser):
         elif v_winner[i] < v_loser[i]: h.append(-1) #L 
         else: h.append(-0) #e
     return h
-
-def error_detection(v_vector, weights, obs_winner):
-    pred_winner = hg.select_winner(v_vector, weights)
-    return pred_winner != obs_winner
 
 def update_weights(ranking, weights, update_rate):
     #ranking = erc(obs_winner, pred_winner)
