1aec47138235c9fb05ad1b4428c30b4e4fcf953f
sautedman <bowersd@email.arizona.edu>
2022-10-02 09:51:11 -0700

    cleaner version of weight update, negative weights avoided

--- weight_learner.py
+++ weight_learner.py
@@ -12,8 +12,13 @@ def error_detection(v_vector, weights, obs_winner):
     pred_winner = hg.select_winner(v_vector, weights)
     return pred_winner != obs_winner
 
-def update_weights(pred_winner, obs_winner, weights, update_rate):
-    ranking = erc(obs_winner, pred_winner)
-    return [weights[i]+(update_rate*ranking[i]) for i in range(len(weights))]
+def update_weights(ranking, weights, update_rate):
+    #ranking = erc(obs_winner, pred_winner)
+    h = []
+    for i in range(len(weights)):
+        update = weights[i]+(update_rate*ranking[i])
+        if update >= 0: h.append(update)
+        else: h.append(0)
+    return h
 
     
