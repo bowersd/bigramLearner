ebb93b917865b2ebddcc238778eca1093a7bec2d
sautedman <bowersd@email.arizona.edu>
2022-10-01 22:43:02 -0700

    function to stochasticize harmonic grammar

--- harmonic_grammar.py
+++ harmonic_grammar.py
@@ -1,12 +1,20 @@
 import random
 
+def perturb_weights(weights):
+    h = []
+    for w in weights:
+        p = random.gauss(w, 2) 
+        if p >= 0: h.append(p)
+        else: append(0)
+    return h
+
 def apply_weights(violations, weights):
     h = []
     for i in range(len(violations)):
         h.append(violations[i]*weights[i])
     return sum(h)
 
-def compete(v_vector, weights):
+def select_winner(v_vector, weights):
     h = []
     floor = 0
     for i in range(len(v_vector)):
