da0ead856a66289877fd56817b2e558d3585c4bb
sautedman <bowersd@email.arizona.edu>
2022-10-01 20:44:34 -0700

    removed superfluous functions

--- harmonic_grammar.py
+++ harmonic_grammar.py
@@ -5,21 +5,12 @@ def apply_weights(violations, weights):
         h.append(violations[i]*weights[i])
     return sum(h)
 
-def best_score(weighted_summed_violations):
-    return max(weighted_summed_violations)
-
-def flag_winner(scored_vectors, top_score):
-    h = []
-    for i in scored vectors:
-
-
 def compete(v_vector, weights):
     h = []
     floor = 0
     for i in range(len(v_vector)):
         scored = apply_weights(v_vector[i])
-        if scored < floor or not floor:
-            h = [i]
+        if scored < floor or not floor: h = [i]
         elif scored == floor: h.append(i)
     return h
 
