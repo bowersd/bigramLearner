6635c3253e9439b5fd62aa23d8df927329973ebc
sautedman <bowersd@email.arizona.edu>
2022-10-01 23:57:55 -0700

    better values for w, e, l

--- weight_learner.py
+++ weight_learner.py
@@ -3,6 +3,6 @@ def erc(v_winner, v_loser):
     h = []
     for i in range(len(v_winner)):
         if v_winner[i] > v_loser[i]: h.append(1) #W
-        elif v_winner[i] < v_loser[i]: h.append(0) #L 
-        else: h.append(-1) #e
+        elif v_winner[i] < v_loser[i]: h.append(-1) #L 
+        else: h.append(-0) #e
     return h
