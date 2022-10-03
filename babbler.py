a2a11a686466dc80ad84405cc09f710ec0f54f57
sautedman <bowersd@email.arizona.edu>
2022-10-02 21:24:01 -0700

    functions to randomly select a word according to frequency

---
+++ babbler.py
@@ -0,0 +1,20 @@
+import random
+
+def laplace(*cnts):
+    return [c+1 for c in cnts]
+
+def cnts_to_cuml_dist(*cnts):
+    h = []
+    total = sum(cnts)
+    cur = 0
+    for c in cnts:
+        cur += c/total
+        h.append(cur)
+    return h
+
+def rand_babble(cuml_dist):
+    i = 0
+    target = random.random()
+    while cuml_dist[i] > target:
+        i += 1
+    return i
