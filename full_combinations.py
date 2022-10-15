c6fb459136f16e8ea4d3e0c1dcf6a14d85b2736d
sautedman <bowersd@email.arizona.edu>
2022-10-15 15:49:59 -0700

    removed natural language prose

--- full_combinations.py
+++ full_combinations.py
@@ -12,5 +12,5 @@ i = 1
 with open("tagalog_prefixes.txt", 'w') as file_out:
     while i <= len(list_of_prefix_slots):
        for c in itertools.combinations(list_of_prefix_slots, i):
-           for p in itertools.product(*c): file_out.write(str(p)+'\n'), but you get the picture
+           for p in itertools.product(*c): file_out.write(str(p)+'\n')
        i += 1
