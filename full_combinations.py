93672e44e5fbe5d37e4e8cd5a72e8a7cd9aee409
sautedman <bowersd@email.arizona.edu>
2022-10-15 15:49:19 -0700

    creating data files

---
+++ full_combinations.py
@@ -0,0 +1,16 @@
+import itertools
+
+list_of_prefix_slots = [
+        ["ma"],
+        ["Pi", "mag"],
+        ["ka", "si"],
+        ["pag"],
+        ["pa"],
+        ["ki"],
+        ]
+i = 1
+with open("tagalog_prefixes.txt", 'w') as file_out:
+    while i <= len(list_of_prefix_slots):
+       for c in itertools.combinations(list_of_prefix_slots, i):
+           for p in itertools.product(*c): file_out.write(str(p)+'\n'), but you get the picture
+       i += 1
